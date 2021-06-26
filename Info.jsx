0ee9638990d15e470cac3f46a790cfafa4ebf0f5
Manula Haputhanthrie <manulahaputhanthrie@gmail.com>
2021-06-27 02:19:35 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and ReactJS</h1>
+      <p>A basic web development bootcamp with JavaScript and ReactJS</p>
+    </div>
+  );
+}
+
+export default Info;
